6d442f8021b68a7a1f10a0e499441bf54332638b
Damian Wróblewski <dwroblewski89@gmail.com>
2026-04-09 22:16:20 +0200

    feat: changed reports availability to job starting date

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,4 +9,5 @@ export const environment = {
 	supabaseUrl: 'https://irogecawricctcorwbpt.supabase.co',
 	supabaseKey: 'sb_publishable_CfYGZaoF0W3C5yQ4e9APBw_pWU5xVp5',
 	vapidPublicKey: 'BKTqtBSrwijRqoFSJFM2_yMQRvLvXq8Hv8awXnp770dXX_waLvZStZXjr80ohkwGMeIllww4eQGCZQtlXZY43IY',
+	allowReportsBeforeJobEnd: false,
 };
